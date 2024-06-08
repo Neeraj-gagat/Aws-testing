3284f68859fed7973958a38758d2292b19fc3072
Neeraj-gagat <neerajgagat9999@gmail.com>
2024-06-08 11:39:42 +0530

    aws-testing

---
+++ src/index.ts
@@ -0,0 +1,34 @@
+
+import { Socket } from "socket.io";
+import http from "http";
+
+import  express from "express";
+import {Server} from "socket.io";
+import { UserManager } from "./managers/UserManager";
+
+const app  = express();
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors:{
+        origin: "*"
+    }
+});
+
+const userManager = new UserManager();
+
+io.on('connection', (socket: Socket) => {
+    console.log("a user connected");
+    userManager.addUser("random name", socket);
+    socket.on("disconnect", () => {
+        console.log("user disconnected")
+        userManager.removeUser(socket.id);
+    })
+});
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+  });
+
+server.listen(3000, () => [
+    console.log('listening on *:3000')
+]);
